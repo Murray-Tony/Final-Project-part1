422af0a743c2eac7c532c2cc0598240844e2342c
Tony Murray <85173110+Murray-Tony@users.noreply.github.com>
2021-11-23 19:11:49 +0000

    t

---
+++ Book.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Book
+{
+
+  public virtual string ISBN {get;set;}
+  public virtual string Title {get;set;}
+  public virtual string AuthorInfo {get;set;}
+  public virtual DateTime PublishedOn{get;set;}
+  public virtual string PublishedBy {get;set;}
+
+  public Book():this("Unknown", "Unknown", "Unknown", DateTime.Today, "Unknown")
+        {
+        }
+  public Book(string isbn):this(isbn, "Unknown", "Unknown", DateTime.Today, "Unknown")
+        {
+        }
+  public Book(string isbn, string title):this(isbn, title,"Unknown", DateTime.Today, "Unknown")
+        {
+        }
+  public Book(string isbn, string title, string authorinfo):this(isbn, title, authorinfo, DateTime.Today, "Unknown")
+        {
+        }
+  public Book(string isbn, string title, string authorinfo, DateTime publishedon, string publishedby)
+        {
+            ISBN = isbn;
+            Title = title;
+            AuthorInfo = authorinfo;
+            PublishedOn=publishedon;
+            PublishedBy=publishedby;
+        }
+  public virtual void Display()
+  {
+    Console.WriteLine("The book '"+ Title+"' was written by "+AuthorInfo+" and published on "+PublishedOn);
+  }
+}
--- main.cs
+++ main.cs
@@ -3,5 +3,7 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    Book book1 = new Book("3974", "The littlest derp", "Tony Murray", Convert.ToDateTime("11/03/1991"), "Publishinghouse");
+    book1.Display();
   }
 }
